90a22163952686333dccef2653b582abd2abbc26
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-07-12 14:53:44 +0530

    Search page in Filter box add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
-  <Box textAlign='center' p='5' fontSize={"lg"} color='gray.600' borderTop='1px' borderColor='gray.100'>
+  <Box textAlign='center' p='5' fontSize={"lg"} color='gray.600' fontWeight={"black"} borderTop='1px' borderColor='gray.100'>
     © 2021 Real Estate, Inc.
   </Box>
 );
